076e9a1805adec8a2f2566f73bed3301b10b5d51
Patrick Stephens <pat@calyptia.com>
2022-03-02 16:39:33 +0000

    Add working GC helm deployment

--- grafana-cloud/deploy-helm.sh
+++ grafana-cloud/deploy-helm.sh
@@ -13,8 +13,13 @@ echo "Setting up Fluent Bit to Grafana Cloud from helm chart in $NAMESPACE"
 helm repo add fluent https://fluent.github.io/helm-charts || helm repo add fluent https://fluent.github.io/helm-charts/
 
 # Set up the appropriate variables and substitute them
+if [[ -f "$SCRIPT_DIR/.env" ]]; then
+    # shellcheck disable=SC1091
+    source "$SCRIPT_DIR/.env"
+fi
+
 # shellcheck disable=SC2016
-envsubst '$GRAFANA_CLOUD_USERNAME,$GRAFANA_CLOUD_APIKEY,$GRAFANA_CLOUD_PROM_URL,$GRAFANA_CLOUD_LOKI_URL' < "$SCRIPT_DIR/values-grafana-cloud.yaml" > "$SCRIPT_DIR/values-grafana-cloud-actual.yaml"
+envsubst '$GRAFANA_CLOUD_PROM_USERNAME,$GRAFANA_CLOUD_LOKI_USERNAME,$GRAFANA_CLOUD_APIKEY,$GRAFANA_CLOUD_PROM_URL,$GRAFANA_CLOUD_LOKI_URL' < "$SCRIPT_DIR/values-grafana-cloud.yaml" > "$SCRIPT_DIR/values-grafana-cloud-actual.yaml"
 
 helm upgrade --install fluent-bit fluent/fluent-bit \
     --namespace "$NAMESPACE" \
